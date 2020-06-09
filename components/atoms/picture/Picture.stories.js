114f8d7a2584c3be3951ffa063c08b046818946f
Trez <victorien.lemarie@hotmail.fr>
2020-06-09 12:15:59 +0200

    #4 update 02 atom picture

--- components/atoms/picture/Picture.stories.js
+++ components/atoms/picture/Picture.stories.js
@@ -1,27 +1,16 @@
 import { storiesOf } from '@storybook/vue'
-// import { action } from '@storybook/addon-actions'
 import Picture from './Picture.vue'
-
-// export default { title: 'Atomes/Pictures' }
-
-// export const PictureLamaSrcLien = () => ({
-//     components: { Picture },
-//     template:
-//         '<Picture src="https://pixnio.com/free-images/2017/11/10/2017-11-10-20-55-30-1200x800.jpg" title="Une super Picture de Lama" alt="un lama super">Lama</Picture>'
-// })
 
 const wrapper = {
     components: { Picture }
 }
 
-storiesOf('Atomes/Picture', module)
-    // .addDecorator(withInfo)
-    .add(
-        'Défaut',
-        () => ({
-            ...wrapper,
-            template:
-                '<Picture src="https://pixnio.com/free-images/2017/11/10/2017-11-10-20-55-30-1200x800.jpg" title="Une super Picture de Lama" alt="un lama super">Lama</Picture>'
-        }),
-        { info: true }
-    )
+storiesOf('Atomes/Picture', module).add(
+    'Défaut',
+    () => ({
+        ...wrapper,
+        template:
+            '<Picture src="https://pixnio.com/free-images/2017/11/10/2017-11-10-20-55-30-1200x800.jpg" title="Une super Picture de Lama" alt="un lama super">Lama</Picture>'
+    }),
+    { info: true }
+)
