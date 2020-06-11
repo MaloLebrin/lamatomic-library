55028b7ec6082904d4fd20fbc647079bf3df63de
Guillaume Merceron <merceron@gmail.com>
2020-06-11 13:58:49 +0200

    WIP Fix vue-typed-js integration

--- components/Molecules/typed/typed.vue
+++ components/Molecules/typed/typed.vue
@@ -1,23 +1,21 @@
 <template>
-    <div ref="typedElement" class="typed-element">
-        <slot></slot>
-    </div>
+    <vue-typed-js :strings="['First text', 'Second Text']">
+        <h1 class="typing"></h1>
+    </vue-typed-js>
 </template>
 
 <script>
-import Typed from 'typed.js'
-import { props, getEventHandlers } from './typed-component.config.js'
+import Vue from 'vue'
+import VueTypedJs from 'vue-typed-js'
+
+Vue.use(VueTypedJs)
 
 export default {
     name: 'Typed',
-    props,
     data() {
         return {
             typedObj: null
         }
-    },
-    mounted() {
-        this.initTypedJS()
     },
     destroyed() {
         this.typedObj.destroy()
@@ -25,19 +23,6 @@ export default {
     methods: {
         throwError(message) {
             throw new TypeError(message)
-        },
-        initTypedJS() {
-            const $typed = this.$refs.typedElement.querySelector('.typing')
-
-            if (this.$slots.default.length > 1) {
-                this.throwError(
-                    `Just one child element allowed inside <${this.$options.name}> component.`
-                )
-            } else if (this.$slots.default.length === 1) {
-                let typedConfig = this.$props
-                typedConfig = getEventHandlers(this, typedConfig)
-                this.typedObj = new Typed($typed, typedConfig)
-            }
         }
     }
 }
