3e97883947a6f4ee551199be46775a41ff3fbed0
Trez <victorien.lemarie@hotmail.fr>
2020-06-22 16:13:52 +0200

    #3 update 04 Logo

--- components/molecules/logo/MLogo.vue
+++ components/molecules/logo/MLogo.vue
@@ -1,19 +1,28 @@
 <template>
-    <ALink to="accueil" title="Logo Lamacompta" external rel="sidebar" no-line>
+    <ALink
+        class="logo-lamacompta"
+        to="accueil"
+        title="Logo Lamacompta"
+        external
+        rel="sidebar"
+        no-line
+    >
+        <!-- <LamaLogo class="logo-lamacompta" /> -->
         <AImage
-            src="https://pixnio.com/free-images/2017/11/10/2017-11-10-20-55-30-1200x800.jpg"
+            src="svg/logo.svg"
             title="Le super Lamalogo"
             alt="Logo Lamacompta"
         />
-        Accueil link
     </ALink>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+// import LamaLogo from 'svg/logo.svg?inline'
 
 export default Vue.extend({
     name: 'MLogo'
+    // components: { LamaLogo }
 })
 </script>
 
@@ -21,95 +30,5 @@ export default Vue.extend({
 .logo-lamacompta {
     margin: auto;
     animation: 1s appear;
-
-    .st0 {
-        fill: none;
-    }
-    .st1 {
-        fill: #ffffff;
-    }
-    .st2 {
-        fill: #009cde;
-    }
-    .st3 {
-        fill: #c8e2e8;
-    }
-    .st4 {
-        display: none;
-        fill: #ffffff;
-    }
-    .st5 {
-        fill: url(#SVGID_1_);
-    }
-    .st6 {
-        fill: url(#SVGID_2_);
-    }
-    .st7 {
-        fill: url(#SVGID_3_);
-    }
-    .st8 {
-        display: none;
-    }
-    .st9 {
-        display: inline;
-        fill: #e8f6f9;
-    }
-    .st10 {
-        display: inline;
-        fill: #b0b2bf;
-    }
-    .st11 {
-        display: inline;
-        fill: #d8ecf3;
-    }
-    .st12 {
-        display: inline;
-        fill: #d9ecf3;
-    }
-    .st13 {
-        display: inline;
-        opacity: 9e-2;
-        fill: #636363;
-    }
-    .st14 {
-        display: inline;
-        fill: #ffffff;
-        stroke: #000000;
-        stroke-miterlimit: 10;
-    }
-    .st15 {
-        display: inline;
-        fill: #aae4e5;
-    }
-    .st16 {
-        display: inline;
-        fill: #8cd0d6;
-    }
-    .st17 {
-        display: inline;
-        opacity: 0.1;
-        fill: #6d6d6d;
-    }
-    .st18 {
-        display: inline;
-    }
-    .st19 {
-        fill: none;
-        stroke: #6d6d6d;
-        stroke-miterlimit: 10;
-    }
-    .st20 {
-        fill: none;
-        stroke: #6d6d6d;
-        stroke-miterlimit: 10;
-        stroke-dasharray: 5.9969, 5.9969;
-    }
-    .st21 {
-        display: inline;
-        fill: none;
-        stroke: #6d6d6d;
-        stroke-miterlimit: 10;
-        stroke-dasharray: 6;
-    }
 }
 </style>
