2abb69b99b36629dda4a0a357d36ad29f8812079
Killian Leroux <killian@lamacompta.co>
2020-06-22 14:54:04 +0200

    Refactoring MTyper

---
+++ components/molecules/typer/MTyper.spec.js
@@ -0,0 +1,30 @@
+import { mount } from '@vue/test-utils'
+import MTyper from './MTyper.vue'
+
+describe('Molecules - MTyper', () => {
+    test('...has tag <vue-typed-js>', () => {
+        const wrapper = mount(MTyper)
+        expect(wrapper.find('vue-typed-js')).toBeTruthy()
+    })
+
+    test('...has strings given', () => {
+        const wrapper = mount(MTyper, {
+            propsData: {
+                strings: ['ohoh', 'héhé', 'hihi'],
+                backSpeed: 10
+            }
+        })
+
+        expect(wrapper.vm.strings).toStrictEqual(['ohoh', 'héhé', 'hihi'])
+    })
+
+    test('...has true for loop prop by default', () => {
+        const wrapper = mount(MTyper)
+        expect(wrapper.vm.loop).toBeTruthy()
+    })
+
+    test('...has 10 for backSpeed prop by default', () => {
+        const wrapper = mount(MTyper)
+        expect(wrapper.vm.backSpeed).toBe(10)
+    })
+})
---
+++ components/molecules/typer/MTyper.stories.js
@@ -0,0 +1,21 @@
+import { storiesOf } from '@storybook/vue'
+import MTyper from './MTyper.vue'
+
+const wrapper = {
+    components: { MTyper }
+}
+
+storiesOf('Molecules/Typer', module).add('Défaut', () => ({
+    ...wrapper,
+    data() {
+        return {
+            strings: [
+                'Café et thé à volonté',
+                'No costume',
+                'International',
+                'Cabinet à impact positif'
+            ]
+        }
+    },
+    template: `<MTyper :strings="strings" />`
+}))
