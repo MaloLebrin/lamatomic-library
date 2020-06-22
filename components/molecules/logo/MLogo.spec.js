d4d63064392e8514a665bc410f8e4deeccc30cac
Trez <victorien.lemarie@hotmail.fr>
2020-06-23 00:03:55 +0200

    #3 updates 06 Logo - Test jest

--- components/molecules/logo/MLogo.spec.js
+++ components/molecules/logo/MLogo.spec.js
@@ -1,9 +1,17 @@
 import { mount } from '@vue/test-utils'
-import MLogo from './MLogo.vue'
+import MLogo from '~/components/molecules/logo/MLogo.vue'
 
 describe('Atom - MLogo', () => {
-    test('...has <svg> tag', () => {
+    test('...molecule MLogo to be', () => {
         const wrapper = mount(MLogo)
-        expect(wrapper.find('svg')).toBeTruthy()
+        expect(wrapper.html()).toBe(
+            `<a class="link no-line="true">
+                <img
+                    :src="/_nuxt/components/molecules/logo/svg/logo.svg"
+                    title="Le super Lamalogo"
+                    alt="Logo Lamacompta"
+                />
+            </a>`
+        )
     })
 })
