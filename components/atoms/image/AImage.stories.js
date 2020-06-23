db400e7442247e889160fa0228eb5f382d7152d4
Guillaume Merceron <merceron@gmail.com>
2020-06-23 18:33:12 +0200

    Fix AImage atom

--- components/atoms/image/AImage.stories.js
+++ components/atoms/image/AImage.stories.js
@@ -5,12 +5,12 @@ const wrapper = {
     components: { AImage }
 }
 
-storiesOf('Atomes/AImage', module).add(
+storiesOf('Atoms/AImage', module).add(
     'Défaut',
     () => ({
         ...wrapper,
         template:
-            '<AImage src="https://pixnio.com/free-images/2017/11/10/2017-11-10-20-55-30-1200x800.jpg" title="Une super AImage de Lama" alt="un lama super" />'
+            '<AImage src="https://placehold.it/350x150" title="Une super AImage de Lama" alt="un lama super" />'
     }),
     { info: true }
 )
