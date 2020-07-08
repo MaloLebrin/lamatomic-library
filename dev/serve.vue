bc2b646213f982782c11b89b2fe323a64a251d6d
Killian Leroux <killian@lamacompta.co>
2020-07-08 21:10:09 +0200

    AText: move to right directory + improve usability

--- dev/serve.vue
+++ dev/serve.vue
@@ -1,19 +1,21 @@
 <script lang="ts">
 import Vue from 'vue';
-import { AButton, AHeading, MLogo } from '@/entry';
+import { AButton, AHeading, MLogo, AText } from '@/entry';
 
 export default Vue.extend({
   name: 'ServeDev',
   components: {
     AButton,
     AHeading,
-    MLogo
+    MLogo,
+    AText
   }
 });
 </script>
 
 <template>
   <div id="app">
+    <AText align="right" weight="bold" italic>Oh yeah!</AText>
     <AButton>Yeah</AButton>
     <AHeading>Gros titre de fou</AHeading>
     <MLogo />
---
+++ src/components/atoms/text/AText.vue
@@ -0,0 +1,148 @@
+<template>
+    <component
+        :is="tag"
+        class="a-text"
+        :class="[align, weight, decoration, {italic}]"
+    >
+        <slot />
+    </component>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+    name: 'AText',
+    props: {
+        align: {
+            type: String,
+            default: null,
+            validator(value) {
+                return ['left', 'right', 'center', 'justify'].includes(value)
+            }
+        },
+
+        weight: {
+            type: String,
+            default: null,
+            validator(value) {
+                return ['thin', 'normal', 'bold', 'bolder'].includes(value)
+            }
+        },
+
+        decoration: {
+            type: String,
+            default: null,
+            validator(value) {
+                return ['no-decoration', 'blink', 'dashed', 'dotted', 'double', 'underline'].includes(value)
+            }
+        },
+
+        italic: {
+            type: Boolean,
+            default: false
+        },
+
+        span: {
+            type: Boolean,
+            default: false
+        }
+    },
+
+    computed: {
+        tag(): String {
+            if (this.span) return 'span'
+            return 'p'
+        }
+    }
+})
+</script>
+
+<style lang="scss">
+.a-text {
+    // Align
+    &.left {
+        text-align: left;
+    }
+
+    &.center {
+        text-align: center;
+    }
+
+    &.right {
+        text-align: right;
+    }
+
+    &.justify {
+        text-align: justify;
+    }
+
+    // Style
+    &.no-style {
+        font-style: none !important;
+    }
+
+    &.italic {
+        font-style: italic;
+    }
+
+    // Weight
+    &.thin {
+        font-weight: 100;
+    }
+
+    &.normal {
+        font-weight: 300;
+    }
+
+    &.bold {
+        font-weight: 600;
+    }
+
+    &.bolder {
+        font-weight: 900;
+    }
+
+    // Decoration
+    &.no-decoration {
+        text-decoration: none !important;
+    }
+
+    &.blink {
+        text-decoration: blink;
+    }
+
+    &.dashed {
+        text-decoration: dashed;
+    }
+
+    &.dotted {
+        text-decoration: dotted;
+    }
+
+    &.double {
+        text-decoration: double;
+    }
+
+    &.underline {
+        text-decoration: underline;
+    }
+
+    // Transform
+    &.no-transform {
+        text-transform: none !important;
+    }
+
+    &.capitalize {
+        text-transform: capitalize;
+    }
+
+    &.lowercase {
+        text-transform: lowercase;
+    }
+
+    &.uppercase {
+        text-transform: uppercase;
+    }
+}
+</style>
