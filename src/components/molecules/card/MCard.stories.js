f76a2fb4ad7bf259719970c26d841ee73651cd63
Victorien <victorien.lemarie@hotmail.fr>
2020-07-08 16:52:20 +0000

    Resolve "Card"

---
+++ src/components/molecules/card/MCard.stories.js
@@ -0,0 +1,25 @@
+import { storiesOf } from '@storybook/vue'
+import MCard from './MCard.vue'
+
+const wrapper = {
+    components: { MCard }
+}
+
+storiesOf('Molecules/Card', module)
+    .add("Card simple", () => ({
+        ...wrapper,
+        template:
+            `<MCard><p>Voici une Card simple</p></MCard>`
+    }))
+
+    .add("Card link", () => ({
+        ...wrapper,
+        template:
+            `<MCard isLink to="contact"> Voici une Card link</MCard>`
+    }))
+
+    .add("Card sans anim", () => ({
+        ...wrapper,
+        template:
+            `<MCard noAnim to="contact"> Une card encore plus simple</MCard>`
+    }))
