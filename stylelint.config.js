2207368c5e609eeed6f8b886f6de4faddb9c27ec
Killian Leroux <killian@lamacompta.co>
2020-07-20 15:17:44 +0200

    Add stylelint and improve yarn lint command

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,5 +1,14 @@
 module.exports = {
+    extends: [
+        'stylelint-config-standard',
+        'stylelint-config-prettier',
+        'stylelint-config-sass-guidelines',
+    ],
     // add your custom config here
     // https://stylelint.io/user-guide/configuration
-    rules: {}
+    rules: {
+        indentation: 4,
+        'max-nesting-depth': 20,
+        'selector-max-id': 5,
+    },
 }
