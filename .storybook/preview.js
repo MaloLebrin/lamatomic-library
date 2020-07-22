2913afd0c490dab2dcb6ca164b87e171fbab38b5
Trez <victorien.lemarie@hotmail.fr>
2020-07-22 13:31:28 +0200

    storybook addon themes init 02

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,7 +3,6 @@ import { addParameters } from '@storybook/vue'
 addParameters({
   themes: [
     { name: 'default', color: '#000000' },
-    { name: 'lamacompta', class: 'lamatheme', color: '#009CDE', default: true },
-    { name: 'spotify', class: 'theme-spotify', color: '#3ac47d' }
+    { name: 'lamacompta', class: 'lamatheme', color: '#009CDE', default: true }
   ],
 });
