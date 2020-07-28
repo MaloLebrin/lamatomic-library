0db1912bea79768269a95eea8e397064a13a8233
Killian Leroux <killian@lamacompta.co>
2020-07-28 16:21:45 +0200

    Fix forget /inputs/ folder rename reference

--- src/components/atoms/form/group/AFormGroup.stories.js
+++ src/components/atoms/form/group/AFormGroup.stories.js
@@ -1,5 +1,5 @@
 import { storiesOf } from '@storybook/vue'
-import AInputText from '../input/input-text/AInputText.vue'
+import AInputText from '../inputs/input-text/AInputText.vue'
 import AFormGroup from './AFormGroup.vue'
 
 const wrapper = {
