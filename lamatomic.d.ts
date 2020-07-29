5d572341584e89b72ce1e0ee32e01bb95c1e333e
Killian Leroux <killian@lamacompta.co>
2020-07-29 11:43:22 +0200

    Update TypeScript Types

--- lamatomic.d.ts
+++ lamatomic.d.ts
@@ -1,11 +1,8 @@
-import Vue, { PluginFunction, VueConstructor } from 'vue';
-
+import { PluginFunction } from 'vue'
 
 interface InstallFunction extends PluginFunction<any> {
-  installed?: boolean;
+    installed?: boolean
 }
 
-declare const Lamatomic: { install: InstallFunction };
-export default Lamatomic;
-
-export const LamatomicSample: VueConstructor<Vue>;
+declare const Lamatomic: { install: InstallFunction }
+export default Lamatomic
