2380d399c133b565cec7a85ac8ea6e52a22f03ad
Killian Leroux <killian@lamacompta.co>
2020-08-26 14:28:32 +0200

    Build 2.0.0-0.1.11: Update Card Background

--- dist/lamatomic.esm.js
+++ dist/lamatomic.esm.js
@@ -2773,8 +2773,8 @@ var __vue_staticRenderFns__$p = [];
 
 const __vue_inject_styles__$p = function (inject) {
   if (!inject) return;
-  inject("data-v-7218d484_0", {
-    source: ".m-card{border-radius:1rem;box-shadow:0 2px 4px rgba(0,0,0,.25);margin-bottom:2rem;overflow:hidden;padding:2rem;transition:all .3s ease-in-out}.m-card.is-link:hover{cursor:pointer}.m-card.is-link:hover a{color:#009cde}.m-card.is-link>.link{bottom:0;color:#333;left:0;position:absolute;right:0;top:0}.m-card:not(.no-anim):hover{box-shadow:0 4px 8px rgba(0,0,0,.25)}",
+  inject("data-v-2e761adc_0", {
+    source: ".m-card{background-color:#fff;border-radius:1rem;box-shadow:0 2px 4px rgba(0,0,0,.25);margin-bottom:2rem;overflow:hidden;padding:2rem;transition:all .3s ease-in-out}.m-card.is-link:hover{cursor:pointer}.m-card.is-link:hover a{color:#009cde}.m-card.is-link>.link{bottom:0;color:#333;left:0;position:absolute;right:0;top:0}.m-card:not(.no-anim):hover{box-shadow:0 4px 8px rgba(0,0,0,.25)}",
     map: undefined,
     media: undefined
   });
