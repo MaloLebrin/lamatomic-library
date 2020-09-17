42db99fce0954b334294130726c732e3d1ff1ce6
Killian Leroux <killian@lamacompta.co>
2020-09-17 16:06:53 +0000

    Update AList.stories.js

--- src/components/atoms/list/AList.stories.js
+++ src/components/atoms/list/AList.stories.js
@@ -12,51 +12,55 @@ storiesOf('Atoms/List', module)
     .add('Vertical with chips (default)', () => ({
         ...wrapper,
         template: `<AList>
-                        <AListItem>default</AListItem>
-                        <AListItem>default</AListItem>
+                        <AListItem>Lapin 🐇</AListItem>
+                        <AListItem>Banane 🍌</AListItem>
+                        <AListItem>Papier toilette 🧻</AListItem>
                     </AList>`,
     }))
 
     .add('Vertical without chips', () => ({
         ...wrapper,
         template: `<AList withoutChips>
-                        <AListItem>default</AListItem>
-                        <AListItem>default</AListItem>
+                        <AListItem>Hérisson 🦔</AListItem>
+                        <AListItem>Pique ♠</AListItem>
+                        <AListItem>Kamoulox 💩</AListItem>
                     </AList>`,
     }))
 
     .add('Vertical, no chips, no padding', () => ({
         ...wrapper,
         template: `<AList withoutChips noPadding>
-                        <AListItem>default</AListItem>
-                        <AListItem>default</AListItem>
-                        <AListItem>default</AListItem>
+                        <AListItem>Ohohoh</AListItem>
+                        <AListItem>Héhéhé</AListItem>
+                        <AListItem>Hihihi</AListItem>
                     </AList>`,
     }))
 
     .add('Horizontal with chips', () => ({
         ...wrapper,
         template: `<AList horizontal>
-                        <AListItem>default</AListItem>
-                        <AListItem>default</AListItem>
-                        <AListItem>default</AListItem>
+                        <AListItem>Poisson 🐠</AListItem>
+                        <AListItem>Rouge 🟥</AListItem>
+                        <AListItem>La mer 🌊</AListItem>
+                        <AListItem>Coquillage 🐚</AListItem>
                     </AList>`,
     }))
 
     .add('Horizontal without chips', () => ({
         ...wrapper,
         template: `<AList horizontal withoutChips>
-                        <AListItem>default</AListItem>
-                        <AListItem>default</AListItem>
-                        <AListItem>default</AListItem>
+                        <AListItem>Evènement 📅</AListItem>
+                        <AListItem>Contact 📞</AListItem>
+                        <AListItem>Protection 😷</AListItem>
                     </AList>`,
     }))
 
     .add('Horizontal, no chips, no padding', () => ({
         ...wrapper,
         template: `<AList horizontal withoutChips noPadding>
-                        <AListItem>default</AListItem>
-                        <AListItem>default</AListItem>
-                        <AListItem>default</AListItem>
+                        <AListItem>Concert 🎙</AListItem>
+                        <AListItem>Evènement 📅</AListItem>
+                        <AListItem>Contact 📞</AListItem>
+                        <AListItem>Protection 😷</AListItem>
                     </AList>`,
     }))
